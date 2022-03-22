3df003839253759404dd0e930bc2062766afe363
KoverevEvgeniy <koverev.evg@gmail.com>
2022-03-22 16:17:44 +0300

    test logs

--- app1/tests.py
+++ app1/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import logging
 
-# Create your tests here.
+from django.http import request
+from django.test import TestCase, tag
+import app1.views
+
+
+class YourTestClass(TestCase):
+    @tag('log1')
+    def test_logging(self):
+        with self.assertLogs() as captured:
+            app1.views.main(request=request)
+        self.assertEqual(len(captured.records), 4)
+        self.assertEqual(captured.records[0].getMessage(), "logger1 - debug")
